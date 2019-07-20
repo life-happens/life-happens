7eeeadd36744981e057243aebd3e0e262d4ad307
Diana <d_quinlan061006@yahoo.com>
2019-07-20 12:52:06 -0400

    added side log in component, need to personalize and style more

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/jumbotron/index";
 import About from "../components/about/index";
+import Login from "../components/login/index";
 class Main extends Component {
 
 render() {
@@ -9,7 +10,9 @@ render() {
     <Jumbotron>
         <h1>Life Happens</h1>
     </Jumbotron>
-    <About></About>
+    <About>
+    </About>
+    <Login></Login>
     </div>
     );
 }
