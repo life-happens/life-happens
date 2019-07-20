6a3365dc25d0930eefbc6e46346a32115b25ebd8
Diana <d_quinlan061006@yahoo.com>
2019-07-20 15:30:38 -0400

    tested css for jumbotron, will return to styling the main page

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 function Jumbotron ({children}) {
     return (
         <div
@@ -9,13 +10,6 @@ function Jumbotron ({children}) {
       </div>
     )
 }
-
-
-
-
-
-
-
 
 
 export default Jumbotron;
