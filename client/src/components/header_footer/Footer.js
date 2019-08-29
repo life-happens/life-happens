fd40628ae8c561e732e2d2d9a9558e1c2aa8f327
Diana <d_quinlan061006@yahoo.com>
2019-08-28 21:30:10 -0400

    added logo to footer

--- client/src/components/header_footer/Footer.js
+++ client/src/components/header_footer/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 // import Grid from '@material-ui/core/Grid';
+import Logo from "../../resources/images/eventize.png"
 
 const Footer = () => {
   return (
     // <Grid container>
     <footer className="bck_dark">
       <Fade delay={500}>
-        <div className="font_righteous footer_logo_venue">Eventize</div>
+        <div className="font_righteous" >
+          <img src={Logo} alt="eventize" style={{maxWidth:250}}/>
+        </div>
         <div className="footer_copyright">
           Eventize 2019. All rights reserved.
         </div>
